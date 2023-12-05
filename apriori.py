682fa40d55e48c812f127e20dd09ee306d2e0b90
Niveditha1406 <85439326+Niveditha1406@users.noreply.github.com>
2023-12-05 17:23:01 +0530

    Create apriori.py

---
+++ apriori.py
@@ -0,0 +1,17 @@
+import numpy as np 
+import pandas as pd 
+from mlxtend.frequent_patterns import apriori, association_rules
+data = pd.read_csv('/content/GroceryStoreDataSet.csv',names=['Products'],header=None)
+data.head()
+data = list(data["Products"].apply(lambda x:x.split(',')))
+data
+from mlxtend.preprocessing import TransactionEncoder
+te = TransactionEncoder()
+te_data = te.fit(data).transform(data)
+df = pd.DataFrame(te_data,columns=te.columns_)
+df.head()
+frq_items = apriori(df, min_support = 0.1, use_colnames = True)
+rules = association_rules(frq_items, metric ="lift", min_threshold = 1) 
+rules = rules.sort_values(['confidence', 'lift'], ascending =[False, False])
+print(rules.head())
+print(rules)
